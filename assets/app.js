d6e8c64c1a4505a3d69069dd8d1abf91cc688213
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-03-06 14:32:07 -0500

    update

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,13 @@
 
-document.getElementById('btn-photo').addEventListener('click', function() {
+
+document.getElementById('btn-photo').addEventListener('click', function(evt) {
+	evt.preventDefault();
 	var editor = document.querySelector(".CodeMirror")
 	html2canvas(editor).then( canvas => {
 		let link = document.createElement('a');
 		link.style.display = 'none';
 		link.setAttribute('href', canvas.toDataURL("image/jpeg"));
-		link.setAttribute('download', 'snippy.jpg');
+		link.setAttribute('download', 'snippy.png');
 
 		document.body.appendChild(link);
 
