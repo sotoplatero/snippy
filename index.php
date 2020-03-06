13e4c69edf05f1276be1caf241c1359e1c0cf9da
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-03-06 12:32:15 -0500

    update

--- index.php
+++ index.php
@@ -1,33 +1,56 @@
 <?php
 require 'vendor/autoload.php';
 
-use Siler\Functional as λ;
 use Siler\Route;
 use Siler\Twig;
 use Siler\Http\Request;
 use Siler\Http\Response;
+	
+$curl = curl_init('https://snippets-f1de.restdb.io/rest/snippets');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+  'Cache-Control: no-cache',
+  'X-Apikey: d9138783c9c70b15a9201763949b9d10d5de5',
+  'Content-Type: application/json'	
+]);
+
 
 Twig\init('app/views');
-// $pdo = new \PDO( 'sqlite:database.sqlite' );
-// $db = new \LessQL\Database( $pdo );	
 
 Route\get('/', function(){
 	echo Twig\render('home.html');
 });
 
-// Route\post('/', function() use ($db) {
+Route\post('/', function() use ($curl) {
 
-// 	$row = $db->createRow('snippets',array(
-// 	    'title' => Request\post('title'),
-// 	    'snippet' => Request\post('code'),
-// 	));	
-// 	$row->save();
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS,  json_encode(['code' => Request\post('code')] ));
 
-// 	Response\redirect('/'.$row->id);
-// });
+	try {
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$response = json_decode($response) ;
 
-// Route\get('/{id}', function( array $routeParams ) use ($db){
-// 	$snippet = $db->snippets()->where('id',$routeParams['id'])->fetch();
-// 	// var_dump($snippet->snippet);
-// 	echo Twig\render( 'edit.twig', ['code'=>$snippet->snippet] );
-// });
+		Response\redirect('/'.$response->_id);
+		
+	} catch (HttpException $ex) {
+	  echo $ex;
+	}
+
+});
+
+Route\get('/{id}', function( array $routeParams ) {
+
+	$curl = curl_init('https://snippets-f1de.restdb.io/rest/snippets/'.$routeParams['id']);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, [
+	  'Cache-Control: no-cache',
+	  'X-Apikey: d9138783c9c70b15a9201763949b9d10d5de5',
+	  'Content-Type: application/json'	
+	]);
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$response = json_decode($response) ;
+
+	echo Twig\render( 'edit.twig', [ 'code' => $response->code ] );
+});
