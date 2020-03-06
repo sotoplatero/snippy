c096e9d1a05845a2b847f65d38adadf5eec2f743
Damian Soto <soto.platero@gmail.com>
2020-03-06 06:46:44 -0500

    UPDATE

--- index.php
+++ index.php
@@ -8,26 +8,26 @@ use Siler\Http\Request;
 use Siler\Http\Response;
 
 Twig\init('app/views');
-$pdo = new \PDO( 'sqlite:database.sqlite' );
-$db = new \LessQL\Database( $pdo );	
+// $pdo = new \PDO( 'sqlite:database.sqlite' );
+// $db = new \LessQL\Database( $pdo );	
 
 Route\get('/', function(){
 	echo Twig\render('home.html');
 });
 
-Route\post('/', function() use ($db) {
+// Route\post('/', function() use ($db) {
 
-	$row = $db->createRow('snippets',array(
-	    'title' => Request\post('title'),
-	    'snippet' => Request\post('code'),
-	));	
-	$row->save();
+// 	$row = $db->createRow('snippets',array(
+// 	    'title' => Request\post('title'),
+// 	    'snippet' => Request\post('code'),
+// 	));	
+// 	$row->save();
 
-	Response\redirect('/'.$row->id);
-});
+// 	Response\redirect('/'.$row->id);
+// });
 
-Route\get('/{id}', function( array $routeParams ) use ($db){
-	$snippet = $db->snippets()->where('id',$routeParams['id'])->fetch();
-	// var_dump($snippet->snippet);
-	echo Twig\render( 'edit.twig', ['code'=>$snippet->snippet] );
-});
+// Route\get('/{id}', function( array $routeParams ) use ($db){
+// 	$snippet = $db->snippets()->where('id',$routeParams['id'])->fetch();
+// 	// var_dump($snippet->snippet);
+// 	echo Twig\render( 'edit.twig', ['code'=>$snippet->snippet] );
+// });
